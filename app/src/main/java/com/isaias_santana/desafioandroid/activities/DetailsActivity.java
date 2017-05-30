e82481dc538a0e6a23b8aa6b0e57d8b1f267a4b3
IsaiasSantana <isds.santana@gmail.com>
2017-05-30 19:38:09 -0300

    Favorite fab color

--- app/src/main/java/com/isaias_santana/desafioandroid/activities/DetailsActivity.java
+++ app/src/main/java/com/isaias_santana/desafioandroid/activities/DetailsActivity.java
@@ -1,9 +1,18 @@
 package com.isaias_santana.desafioandroid.activities;
 
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.isaias_santana.desafioandroid.R;
@@ -13,6 +22,9 @@ import com.isaias_santana.desafioandroid.domain.People;
  * Created by isaias on 29/05/17.
  */
 public class DetailsActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+    private boolean isFavorite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,11 +40,15 @@ public class DetailsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         initViews(p);
 
+        isFavorite = false;
+
         Log.d("people",p.getName());
     }
 
     private void initViews(People p)
     {
+        //Campos de texto
+
         ((TextView) findViewById(R.id.tv_nome_personagem)).setText(p.getName());
         ((TextView) findViewById(R.id.tv_detalhes_planeta_natal)).setText(p.getHomeWorld());
         ((TextView) findViewById(R.id.tv_detalhes_especie)).setText(p.getSpecies().get(0));
@@ -43,6 +59,33 @@ public class DetailsActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.tv_detalhes_cabelo)).setText(p.getHairColor());
         ((TextView) findViewById(R.id.tv_detalhes_cor_pele)).setText(p.getSkinColor());
         ((TextView) findViewById(R.id.tv_detalhes_olhos)).setText(p.getEyeColor());
+
+        //Floating Action Button
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(!isFavorite)
+                {
+                    int color = ContextCompat.getColor(getContext(),android.R.color.holo_orange_light);
+                    DrawableCompat.setTint(DrawableCompat.wrap(fab.getDrawable()), color);
+                    isFavorite = true;
+                }
+                else{
+                    int color = ContextCompat.getColor(getContext(),android.R.color.darker_gray);
+
+                    DrawableCompat.setTint(DrawableCompat.wrap(fab.getDrawable()), color);
+                    isFavorite = false;
+                }
+            }
+        });
+
     }
 
+    private Context getContext(){
+        return this;
+    }
 }
